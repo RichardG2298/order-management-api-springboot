71c1b57bf6e272ef8406367aae4234e9f7d83581
Richard Gualancañay <rgualancanayvillalba@gmail.com>
2026-04-08 12:33:09 -0500

    New class for CreateProductUseCase

---
+++ src/main/java/com/richard/order_management_api/application/usecase/CreateProductUseCase.java
@@ -0,0 +1,23 @@
+package com.richard.order_management_api.application.usecase;
+
+import com.richard.order_management_api.application.dto.CreateProductRequest;
+import com.richard.order_management_api.domain.model.Product;
+import com.richard.order_management_api.domain.repository.ProductRepository;
+
+public class CreateProductUseCase {
+    private final ProductRepository productRepository;
+
+    public CreateProductUseCase(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product execute(CreateProductRequest request){
+        Product product = new Product(
+                null,
+                request.getName(),
+                request.getPrice(),
+                request.getStock()
+        );
+        return productRepository.save(product);
+    }
+}
