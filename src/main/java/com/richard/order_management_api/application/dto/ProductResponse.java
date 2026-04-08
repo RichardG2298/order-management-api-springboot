e384db284d280077aa24007988bb95e509e29ad2
Richard Gualancañay <rgualancanayvillalba@gmail.com>
2026-04-08 13:20:23 -0500

    Add annotation AllArgsConstructor

--- src/main/java/com/richard/order_management_api/application/dto/ProductResponse.java
+++ src/main/java/com/richard/order_management_api/application/dto/ProductResponse.java
@@ -1,20 +1,14 @@
 package com.richard.order_management_api.application.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.math.BigDecimal;
 
 @Getter
+@AllArgsConstructor
 public class ProductResponse {
     private Long id;
     private String name;
     private BigDecimal price;
-
-    public ProductResponse() {}
-
-    public ProductResponse(Long id, String name, BigDecimal price) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-    }
 }
