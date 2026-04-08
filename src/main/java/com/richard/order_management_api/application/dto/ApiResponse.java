ece4b0b3eb2d19ebcb0b68d8995566d66809050a
Richard Gualancañay <rgualancanayvillalba@gmail.com>
2026-04-08 16:20:40 -0500

    New method for errors

--- src/main/java/com/richard/order_management_api/application/dto/ApiResponse.java
+++ src/main/java/com/richard/order_management_api/application/dto/ApiResponse.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 import java.time.LocalDateTime;
 
 @Getter
-@JsonPropertyOrder({ "status", "message", "code", "data" })
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"status", "message", "code", "timestamp", "path", "data", "errors"})
 public class ApiResponse<T> {
     private final int status;
     private final String message;
@@ -15,22 +16,28 @@ public class ApiResponse<T> {
     private final LocalDateTime timestamp;
     private final String path;
     private final T data;
+    private final Object errors;
 
-    private ApiResponse(int status, String message, String code, T data, String path) {
+    private ApiResponse(int status, String message, String code, String path, T data, Object errors) {
         this.status = status;
         this.message = message;
         this.code = code;
         this.data = data;
+        this.errors = errors;
         this.path = path;
         this.timestamp = LocalDateTime.now();
     }
 
-    public static <T> ApiResponse<T> success(int status, String message, T data) {
-        return new ApiResponse<>(status, message, null, data, null);
+    public static <T> ApiResponse<T> success(int status, String message, String path, T data) {
+        return new ApiResponse<>(status, message, null, path, data, null);
     }
 
     public static <T> ApiResponse<T> error(int status, String message, String code, String path) {
-        return new ApiResponse<>(status, message, code, null, path);
+        return new ApiResponse<>(status, message, code, path, null, null);
+    }
+
+    public static <T> ApiResponse<T> error(int status, String message, String code, String path, Object errors) {
+        return new ApiResponse<>(status, message, code, path, null, errors);
     }
 
 }
