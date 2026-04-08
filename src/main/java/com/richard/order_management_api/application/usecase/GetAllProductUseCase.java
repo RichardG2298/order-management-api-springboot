4625bfa2b2cb2a54de43e5a2d57e3327317c0709
Richard Gualancañay <rgualancanayvillalba@gmail.com>
2026-04-08 12:33:16 -0500

    New class for GetAllProductsUseCase

---
+++ src/main/java/com/richard/order_management_api/application/usecase/GetAllProductUseCase.java
@@ -0,0 +1,23 @@
+package com.richard.order_management_api.application.usecase;
+
+import com.richard.order_management_api.domain.model.Product;
+import com.richard.order_management_api.domain.repository.ProductRepository;
+import com.richard.order_management_api.web.exception.ProductsNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+public class GetAllProductUseCase {
+    private final ProductRepository productRepository;
+    public GetAllProductUseCase(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Page<Product> execute(Pageable pageable) {
+        Page<Product> productsPage = productRepository.findAll(pageable);
+        if (productsPage.isEmpty()) {
+            throw new ProductsNotFoundException();
+        }
+        return productsPage;
+    }
+}
