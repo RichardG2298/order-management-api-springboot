939f2f09ab353401291d149ce93cbf849fdae441
Richard Gualancañay <rgualancanayvillalba@gmail.com>
2026-04-08 13:08:21 -0500

    Change API error response

--- src/main/java/com/richard/order_management_api/web/exception/GlobalExceptionHandler.java
+++ src/main/java/com/richard/order_management_api/web/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.richard.order_management_api.web.exception;
 
 import com.richard.order_management_api.application.dto.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -14,28 +15,34 @@ public class GlobalExceptionHandler {
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<ApiResponse<Void>> handleBusiness(BusinessException ex) {
+    public ResponseEntity<ApiResponse<Void>> handleBusinessException(BusinessException ex, HttpServletRequest request) {
         log.warn("Business exception: {}", ex.getMessage());
-        return ResponseEntity
-                .status(ex.getStatus())
-                .body(new ApiResponse<>(
-                        ex.getStatus().value(),
-                        ex.getMessage(),
-                        ex.getCode(),
-                        null
-                ));
+        ApiResponse<Void> response = ApiResponse.error(
+                ex.getStatus().value(),
+                ex.getMessage(),
+                ex.getCode(),
+                request.getRequestURI()
+        );
+        return new ResponseEntity<>(response, ex.getStatus());
+
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse<Void>> handleGeneric(Exception ex) {
+    public ResponseEntity<ApiResponse<Void>> handleGenericException(
+            Exception ex,
+            HttpServletRequest request) {
+
         log.error("Unexpected error occurred", ex);
+
+        ApiResponse<Void> response = ApiResponse.error(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Internal server error",
+                "INTERNAL_ERROR",
+                request.getRequestURI()
+        );
+
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ApiResponse<>(
-                        500,
-                        "Internal server error",
-                        "INTERNAL_ERROR",
-                        null
-                ));
+                .body(response);
     }
 }
