4975c82ec95546b4a18461c386f3f2027060eed5
Richard Gualancañay <rgualancanayvillalba@gmail.com>
2026-04-08 12:26:56 -0500

    Create ProductRepositoryImpl for db connection

---
+++ src/main/java/com/richard/order_management_api/infrastructure/persistence/ProductRepositoryImpl.java
@@ -0,0 +1,48 @@
+package com.richard.order_management_api.infrastructure.persistence;
+
+import com.richard.order_management_api.domain.model.Product;
+import com.richard.order_management_api.domain.repository.ProductRepository;
+import com.richard.order_management_api.infrastructure.persistence.entity.ProductEntity;
+import com.richard.order_management_api.infrastructure.persistence.mapper.ProductMapper;
+import com.richard.order_management_api.infrastructure.persistence.repository.JpaProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Repository
+public class ProductRepositoryImpl implements ProductRepository {
+
+    private final JpaProductRepository jpaRepository;
+
+    public ProductRepositoryImpl(JpaProductRepository jpaRepository) {
+        this.jpaRepository = jpaRepository;
+    }
+
+    @Override
+    public Product save(Product product) {
+        ProductEntity entity = ProductMapper.toEntity(product);
+        ProductEntity saved = jpaRepository.save(entity);
+        return ProductMapper.toDomain(saved);
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        return jpaRepository.findById(id)
+                .map(ProductMapper::toDomain);
+    }
+
+    @Override
+    public Page<Product> findAll(Pageable pageable) {
+        Page<ProductEntity> entitiesPage = jpaRepository.findAll(pageable); // devuelve Page<ProductEntity>
+        return entitiesPage.map(ProductMapper::toDomain); // convierte Page<ProductEntity> → Page<Product>
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        jpaRepository.deleteById(id);
+    }
+}
