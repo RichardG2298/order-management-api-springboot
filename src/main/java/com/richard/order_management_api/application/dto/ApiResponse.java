7bcec246d2e8d399b290658404384f5ad30073e1
Richard Gualancañay <rgualancanayvillalba@gmail.com>
2026-04-08 13:07:51 -0500

    Refactor ApiResponse implementing a factory method

--- src/main/java/com/richard/order_management_api/application/dto/ApiResponse.java
+++ src/main/java/com/richard/order_management_api/application/dto/ApiResponse.java
@@ -4,28 +4,33 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @JsonPropertyOrder({ "status", "message", "code", "data" })
 public class ApiResponse<T> {
     private final int status;
     private final String message;
     private final String code;
+    private final LocalDateTime timestamp;
+    private final String path;
     private final T data;
 
-    //Error
-    public ApiResponse(int status, String message, String code, T data) {
+    private ApiResponse(int status, String message, String code, T data, String path) {
         this.status = status;
         this.message = message;
         this.code = code;
         this.data = data;
+        this.path = path;
+        this.timestamp = LocalDateTime.now();
     }
 
-    //Success
-    public ApiResponse(int status, String message, T data) {
-        this.status = status;
-        this.message = message;
-        this.data = data;
-        this.code = null;
+    public static <T> ApiResponse<T> success(int status, String message, T data) {
+        return new ApiResponse<>(status, message, null, data, null);
+    }
+
+    public static <T> ApiResponse<T> error(int status, String message, String code, String path) {
+        return new ApiResponse<>(status, message, code, null, path);
     }
 
 }
