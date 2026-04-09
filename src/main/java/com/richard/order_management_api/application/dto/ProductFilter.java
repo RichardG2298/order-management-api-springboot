8d12d07643ff3bb780005b19864976b0ae50e0cb
Richard Gualancañay <rgualancanayvillalba@gmail.com>
2026-04-09 15:48:49 -0500

    refactor: Add annotations refer to constructor to simplify the code

--- src/main/java/com/richard/order_management_api/application/dto/ProductFilter.java
+++ src/main/java/com/richard/order_management_api/application/dto/ProductFilter.java
@@ -1,10 +1,15 @@
 package com.richard.order_management_api.application.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductFilter {
     private String name;
     private Boolean active;
