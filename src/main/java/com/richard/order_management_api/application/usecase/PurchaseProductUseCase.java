996d51e73da61c6806b96e021909698559b35c9c
Richard Gualancañay <rgualancanayvillalba@gmail.com>
2026-04-09 15:49:29 -0500

    refactor: Change the object response

--- src/main/java/com/richard/order_management_api/application/usecase/PurchaseProductUseCase.java
+++ src/main/java/com/richard/order_management_api/application/usecase/PurchaseProductUseCase.java
@@ -1,25 +1,32 @@
 package com.richard.order_management_api.application.usecase;
 
+import com.richard.order_management_api.application.dto.ProductResponse;
 import com.richard.order_management_api.application.dto.PurchaseRequest;
 import com.richard.order_management_api.domain.model.Product;
 import com.richard.order_management_api.domain.repository.ProductRepository;
+import com.richard.order_management_api.infrastructure.persistence.mapper.ProductMapper;
 import com.richard.order_management_api.web.exception.InactiveProductException;
 import com.richard.order_management_api.web.exception.ProductAlreadyDeletedException;
 import com.richard.order_management_api.web.exception.ProductNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PurchaseProductUseCase {
     private final ProductRepository productRepository;
 
-    public Product execute(PurchaseRequest  purchaseRequest) {
+    @Transactional
+    public ProductResponse execute(PurchaseRequest  purchaseRequest) {
         Product product = productRepository.findById(purchaseRequest.getProductId())
                 .orElseThrow(() -> new ProductNotFoundException(purchaseRequest.getProductId()));
 
+        product.validateIsActive();
+
         product.decreaseStock(purchaseRequest.getQuantity());
-        return productRepository.save(product);
+        Product updatedProduct = productRepository.save(product);
+        return ProductMapper.toResponse(updatedProduct);
 
 
     }
