b322ffb1f8e7b75d2958728a576ae6d16caadfbc
Richard Gualancañay <rgualancanayvillalba@gmail.com>
2026-04-09 15:50:17 -0500

    feat: Add the @Transactional to avoid errors

--- src/main/java/com/richard/order_management_api/application/usecase/DeleteProductUseCase.java
+++ src/main/java/com/richard/order_management_api/application/usecase/DeleteProductUseCase.java
@@ -3,6 +3,7 @@ package com.richard.order_management_api.application.usecase;
 import com.richard.order_management_api.domain.model.Product;
 import com.richard.order_management_api.domain.repository.ProductRepository;
 import com.richard.order_management_api.web.exception.ProductNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ public class DeleteProductUseCase {
 
     private final ProductRepository productRepository;
 
+    @Transactional
     public void execute(Long id){
         Product product =  productRepository.findById(id)
                 .orElseThrow(() -> new ProductNotFoundException(id));
