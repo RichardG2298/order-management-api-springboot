ec25d5f31d9091d332b1a53a1b2034e564601469
Richard Gualancañay <rgualancanayvillalba@gmail.com>
2026-04-09 15:47:31 -0500

    feat: PageResponse class that sends in the body response

--- src/main/java/com/richard/order_management_api/application/dto/PageResponse.java
+++ src/main/java/com/richard/order_management_api/application/dto/PageResponse.java
@@ -1,22 +1,16 @@
 package com.richard.order_management_api.application.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
 public class PageResponse<T> {
-    private List<T> items;
-    private int pageNumber;
-    private int pageSize;
+    private List<T> content;
+    private int page;
+    private int size;
     private int totalElements;
     private int totalPages;
-
-    public PageResponse(List<T> items, int pageNumber, int pageSize, int totalElements, int totalPages) {
-        this.items = items;
-        this.pageNumber = pageNumber;
-        this.pageSize = pageSize;
-        this.totalElements = totalElements;
-        this.totalPages = totalPages;
-    }
 }
