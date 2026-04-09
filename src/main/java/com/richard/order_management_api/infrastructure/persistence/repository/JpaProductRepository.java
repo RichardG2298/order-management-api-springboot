9b3f5df0107d05be541e7dbccf810b0cf8ff7732
Richard Gualancañay <rgualancanayvillalba@gmail.com>
2026-04-09 12:26:58 -0500

    feat: add new function findAllWithFilters to get all products

--- src/main/java/com/richard/order_management_api/infrastructure/persistence/repository/JpaProductRepository.java
+++ src/main/java/com/richard/order_management_api/infrastructure/persistence/repository/JpaProductRepository.java
@@ -1,9 +1,28 @@
 package com.richard.order_management_api.infrastructure.persistence.repository;
 
-import com.richard.order_management_api.domain.model.Product;
-import com.richard.order_management_api.domain.repository.ProductRepository;
 import com.richard.order_management_api.infrastructure.persistence.entity.ProductEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface JpaProductRepository extends JpaRepository<ProductEntity, Long> {
+    @Query(
+            value = """
+        SELECT p FROM ProductEntity p
+        WHERE (:name IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%')))
+          AND (:active IS NULL OR p.active = :active)
+    """,
+            countQuery = """
+        SELECT COUNT(p) FROM ProductEntity p
+        WHERE (:name IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%')))
+          AND (:active IS NULL OR p.active = :active)
+    """
+    )
+    Page<ProductEntity> findAllWithFilters(
+            @Param("name") String name,
+            @Param("active") Boolean active,
+            Pageable pageable
+    );
 }
