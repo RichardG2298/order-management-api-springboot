d3ddfc6e98d61f1cd72f6fa12db52a0a9f41fd2e
Richard Gualancañay <rgualancanayvillalba@gmail.com>
2026-04-09 12:33:07 -0500

    feat: map active field in product response

--- src/main/java/com/richard/order_management_api/infrastructure/persistence/mapper/ProductMapper.java
+++ src/main/java/com/richard/order_management_api/infrastructure/persistence/mapper/ProductMapper.java
@@ -4,38 +4,36 @@ import com.richard.order_management_api.application.dto.ProductResponse;
 import com.richard.order_management_api.domain.model.Product;
 import com.richard.order_management_api.infrastructure.persistence.entity.ProductEntity;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class ProductMapper {
 
+    // Entity → Domain
     public static Product toDomain(ProductEntity entity){
         return new Product(
                 entity.getId(),
                 entity.getName(),
                 entity.getPrice(),
-                entity.getStock()
+                entity.getStock(),
+                entity.isActive()
         );
     }
 
+    // Domain → Entity
     public static ProductEntity toEntity(Product product){
         ProductEntity entity = new ProductEntity();
         entity.setId(product.getId());
         entity.setName(product.getName());
         entity.setPrice(product.getPrice());
         entity.setStock(product.getStock());
+        entity.setActive(product.isActive());
         return entity;
     }
 
+    // Domain → DTO
     public static ProductResponse toResponse(Product product){
         return new ProductResponse(
                 product.getId(),
                 product.getName(),
                 product.getPrice()
         );
-    }
-
-    public static List<ProductResponse> toResponse(List<Product> products){
-        return products.stream().map(ProductMapper::toResponse).collect(Collectors.toList());
     }
 }
