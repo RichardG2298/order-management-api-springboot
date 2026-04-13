57186ff8dd41ce2c3d1154d634e63e8ed42b653b
Richard Gualancañay <rgualancanayvillalba@gmail.com>
2026-04-13 14:46:00 -0500

    refactor: replace @Setter and @Getter to @Data in UpdateProductRequest

--- src/main/java/com/richard/order_management_api/application/dto/UpdateProductRequest.java
+++ src/main/java/com/richard/order_management_api/application/dto/UpdateProductRequest.java
@@ -2,13 +2,13 @@ package com.richard.order_management_api.application.dto;
 
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
-@Getter
-@Setter
+@Data
 public class UpdateProductRequest {
     @Size(min = 2, message = "Name must have at least 2 characters")
     private String name;
