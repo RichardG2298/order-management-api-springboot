ab3bfd1149b9e3e87733c68c5a944b98cfaa4268
Richard Gualancañay <rgualancanayvillalba@gmail.com>
2026-04-13 11:31:01 -0500

    test: add test for product controller

---
+++ src/test/java/com/richard/order_management_api/web/controller/ProductControllerTest.java
@@ -0,0 +1,44 @@
+package com.richard.order_management_api.web.controller;
+
+import com.richard.order_management_api.application.dto.ProductResponse;
+import com.richard.order_management_api.application.usecase.GetByIdProductUseCase;
+import com.richard.order_management_api.data.ProductTestData;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.assertj.MockMvcTester.MockMvcRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private GetByIdProductUseCase useCase;
+
+    @Test
+    void shouldReturnProduct() throws Exception {
+        //When
+        when(useCase.execute(1L)).thenReturn(ProductTestData.ProductResponse());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/products/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.data.id").value(1))
+                .andExpect(jsonPath("$.data.name").value("Laptop"))
+                .andExpect(jsonPath("$.data.price").value(10))
+                .andExpect(jsonPath("$.data.stock").value(10));
+
+    }
+}
