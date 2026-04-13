c4cbe5c0c9c2f83c945a597e2adb9b45d79bf44e
Richard Gualancañay <rgualancanayvillalba@gmail.com>
2026-04-13 16:21:58 -0500

    feat: add JwtService for creating of tokens

---
+++ src/main/java/com/richard/order_management_api/infrastructure/security/JwtService.java
@@ -0,0 +1,51 @@
+package com.richard.order_management_api.infrastructure.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET = "my-super-secret-key-that-is-very-long-12345";
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hora
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    public String generateJwtToken(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            extractAllClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
