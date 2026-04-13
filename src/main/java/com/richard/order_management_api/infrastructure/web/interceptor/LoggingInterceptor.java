7d8adc7493d319686cdd92eedfc1ccd4d44c50db
Richard Gualancañay <rgualancanayvillalba@gmail.com>
2026-04-13 14:43:34 -0500

    chore: add logging interceptor handler

---
+++ src/main/java/com/richard/order_management_api/infrastructure/web/interceptor/LoggingInterceptor.java
@@ -0,0 +1,25 @@
+package com.richard.order_management_api.infrastructure.web.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+    private static final Logger log = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("{} {}", request.getMethod(), request.getRequestURI());
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        log.info("Response status: {}", response.getStatus());
+    }
+}
