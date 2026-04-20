00d195bda4c847a1794c14994492e76d0b333ac3
Richard Gualancañay <rgualancanayvillalba@gmail.com>
2026-04-20 12:29:31 -0500

    feat: add new generate refresh token

--- src/main/java/com/richard/order_management_api/infrastructure/security/JwtService.java
+++ src/main/java/com/richard/order_management_api/infrastructure/security/JwtService.java
@@ -1,29 +1,42 @@
 package com.richard.order_management_api.infrastructure.security;
 
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.security.Key;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class JwtService {
 
     private static final String SECRET = "my-super-secret-key-that-is-very-long-12345";
-    private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hora
+    private static final long REFRESH_EXPIRATION = 1000L * 60 * 60 * 24;
+    private static final long ACCESS_EXPIRATION = 1000 * 60 * 60; // 1 hora
+
 
     private SecretKey getSigningKey() {
         return Keys.hmacShaKeyFor(SECRET.getBytes());
     }
 
-    public String generateJwtToken(String username) {
+    public String generateAccessToken(String username, List<String> roles) {
+        return Jwts.builder()
+                .subject(username)
+                .claim("roles", roles)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + ACCESS_EXPIRATION))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String generateRefreshToken(String username) {
         return Jwts.builder()
                 .subject(username)
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .expiration(new Date(System.currentTimeMillis() + REFRESH_EXPIRATION))
                 .signWith(getSigningKey())
                 .compact();
     }
@@ -32,13 +45,14 @@ public class JwtService {
         return extractAllClaims(token).getSubject();
     }
 
-    public boolean isTokenValid(String token) {
-        try {
-            extractAllClaims(token);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
+    public List<String> extractRoles(String token) {
+        Claims claims = extractAllClaims(token);
+
+        List<?> roles = claims.get("roles", List.class);
+
+        return roles.stream()
+                .map(Object::toString)
+                .toList();
     }
 
     private Claims extractAllClaims(String token) {
