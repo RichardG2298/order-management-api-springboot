93c99a10c722219824e3e869a1ba4a7afcb6d644
Richard Gualancañay <rgualancanayvillalba@gmail.com>
2026-04-20 12:20:51 -0500

    feat: add new User entity

---
+++ src/main/java/com/richard/order_management_api/infrastructure/persistence/entity/UserEntity.java
@@ -0,0 +1,29 @@
+package com.richard.order_management_api.infrastructure.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles;
+}
