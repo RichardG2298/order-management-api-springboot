70fb8f2b9895546b297196e587e7cdcdf66e64b1
Richard Gualancañay <rgualancanayvillalba@gmail.com>
2026-04-20 12:24:45 -0500

    feat(auth): create new refresh token service

---
+++ src/main/java/com/richard/order_management_api/infrastructure/security/RefreshTokenService.java
@@ -0,0 +1,49 @@
+package com.richard.order_management_api.infrastructure.security;
+
+import com.richard.order_management_api.infrastructure.persistence.entity.RefreshTokenEntity;
+import com.richard.order_management_api.infrastructure.persistence.repository.JpaRefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+    private final JpaRefreshTokenRepository repository;
+
+    public RefreshTokenEntity create(String token, String username, LocalDateTime expiredAt) {
+        RefreshTokenEntity refreshToken = new RefreshTokenEntity();
+        refreshToken.setToken(token);
+        refreshToken.setUsername(username);
+        refreshToken.setExpiration(expiredAt);
+        refreshToken.setRevoked(false);
+
+        return repository.save(refreshToken);
+    }
+
+    public RefreshTokenEntity validate(String token) {
+        RefreshTokenEntity entity = repository.findByTokenAndRevokedFalse(token)
+                .orElseThrow(() -> new RuntimeException("Invalid refresh token"));
+
+        if(entity.getExpiration().isBefore(LocalDateTime.now())) {
+            throw new RuntimeException("Invalid refresh token");
+        }
+
+        return entity;
+    }
+
+    public void revoke(String token) {
+        RefreshTokenEntity entity = repository.findByToken(token)
+                .orElseThrow(() -> new RuntimeException("Invalid refresh token"));
+
+        entity.setRevoked(true);
+        repository.save(entity);
+    }
+
+    public void revokeAllByUsername(String username) {
+        repository.revokeAllByUsername(username);
+    }
+}
